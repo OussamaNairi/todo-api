39f39eff9f58e6bb782bb977499cb078c41e878c
Oussama Nairi <nairioussama@live.fr>
2020-04-10 19:08:39 +0100

    update

---
+++ todoa-api/src/main/java/com/abolkog/springboot_essat/todo_api/TodoApplication.java
@@ -0,0 +1,17 @@
+package com.abolkog.springboot_essat.todo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class TodoApplication 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(TodoApplication.class,args);
+    }
+}
